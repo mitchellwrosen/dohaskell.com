ed92b1fca05a89ae7495b70df34f202e220f77dd
Mitchell Rosen <mitchellwrosen@gmail.com>
2013-07-28 19:23:13 -0700

    added random module name generator

---
+++ System/Random/Extras.hs
@@ -0,0 +1,19 @@
+module System.Random.Extras
+    ( randomModuleName
+    ) where
+
+import Control.Applicative ((<$>))
+import Control.Monad.Random (getRandomR, getRandomRs)
+import Data.Char (isAlpha)
+
+randomModuleName :: Int -> IO [Char]
+randomModuleName n = do
+    x  <- take 1     <$> randomCapitalLetters
+    xs <- take (n-1) <$> randomLetters
+    return (x ++ xs)
+
+randomCapitalLetters :: IO [Char]
+randomCapitalLetters = getRandomRs ('A', 'Z')
+
+randomLetters :: IO [Char]
+randomLetters = filter isAlpha <$> getRandomRs ('A', 'z')
