8c0f24cda48db07cc57ba64a1726966e6db7079d
Christopher Hebert <hebert.christopherj@gmail.com>
2013-08-31 18:37:47 -0700

    changes

--- Handler/Function.hs
+++ Handler/Function.hs
@@ -2,13 +2,16 @@ module Handler.Function where
 
 import Import
 
+codeForm :: Form Article
+codeForm = renderDivs $ areq textArea "codeArea" (Just "-- Insert code here:")
+
 getFunctionR :: ModuleName -> FunctionName -> Handler Html
 getFunctionR moduleName functionName = do
-    addScriptRemote "hello.jpg"
-    {-addScript $ StaticR codemirror_lib_codemirror_js-}
-    --addStylesheet $ StaticR codemirror_lib_codemirror_css
-    --addScript $ StaticR codemirror_haskell_haskell_js
     defaultLayout $ do
+        addScriptRemote "http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"
+        addScript $ StaticR codemirror_lib_codemirror_js
+        addStylesheet $ StaticR codemirror_lib_codemirror_css
+        addScript $ StaticR codemirror_haskell_haskell_js
         setTitle "DoHaskell!"
         $(widgetFile "function")
 
